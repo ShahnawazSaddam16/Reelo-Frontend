1c82377772676418ea5d741c0fdd6ecf7e747df1
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-15 01:01:21 +0500

    changing in project folder

--- app/screens/AuthScreen.js
+++ app/screens/AuthScreen.js
@@ -4,9 +4,8 @@ import { View, Text, StatusBar } from "react-native";
 export default function AuthScreen() {
   return (
     <>
-     <View className="flex justify-center items-center bg-[#0E0E10]">
+     <View className="flex-1 justify-center items-center bg-[#0E0E10]">
         <StatusBar style="light"/>
-        <Text>Welcome to AuthScreen</Text>
     </View> 
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./App.js", "./components/**/*.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/components/**/*.{js,jsx,ts,tsx}",
+    "./app/screens/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
