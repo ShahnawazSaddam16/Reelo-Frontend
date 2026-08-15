50c4c2733dae7a60b2a2d57fd594ed0b96e806e8
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-15 23:43:52 +0500

    creatng the creating profle component file

---
+++ app/components/Profile/CreatingProfile.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function CreatingProfile() {
+  return (
+    <>
+     <View className="flex">
+        <Text className="text-white text-[16px]">Creating the profile</Text>
+    </View> 
+    </>
+  )
+}
--- app/screens/ProfileScreens/CreatingProfileScreen.js
+++ app/screens/ProfileScreens/CreatingProfileScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { StatusBar, View, Text } from 'react-native'
+import CreatingProfile from '../../components/Profile/CreatingProfile'
 
 export default function CreatingProfileScreen() {
   return (
     <>
      <View className="flex-1 justify-center items-center bg-[#0E0E10]">
         <StatusBar style="light"/>
-        <Text className="text-white">Creating Profile Screen</Text>
+        <CreatingProfile />
     </View> 
     </>
   )
